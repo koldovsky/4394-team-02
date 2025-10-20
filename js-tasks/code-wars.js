9b1c63310fcbb9fe027532ab4d3c54a26b91f0ef
Yuliia <julianna197@gmail.com>
2025-10-20 14:08:30 +0000

    JS tasks

---
+++ js-tasks/code-wars.js
@@ -0,0 +1,46 @@
+// task 1
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+
+// Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+    // Code?
+    if (num <= 0) {
+        return num;
+    }
+    return num * (-1);
+}
+
+// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move(position, roll) {
+    // return the new position
+    return position + (roll * 2);
+}
+
+// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet(name, owner) {
+    if (name == owner) {
+        return 'Hello boss';
+    }
+    return 'Hello guest';
+    // Add code here
+}
+
+// Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+    return parseInt(time / 2);
+}
+
+// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+function lovefunc(flower1, flower2) {
+    // moment of truth
+    return (flower1 % 2 === 0 && flower2 % 2 === 1) || ((flower2 % 2 === 0 && flower1 % 2 === 1));
+}
