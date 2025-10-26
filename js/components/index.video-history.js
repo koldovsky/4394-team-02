ee8f560f52d32322c6c96c62142358e711ce60c2
Yuliia <julianna197@gmail.com>
2025-10-26 18:17:14 +0000

    Add play history video

--- js/components/index.video-history.js
+++ js/components/index.video-history.js
@@ -1,3 +1,9 @@
 function playVideo() {
+    const playVideoInBackground = document.querySelectorAll('.history__video-wrapper');
 
+    playVideoInBackground.forEach(playButton => {
+        playButton.addEventListener('click', function (event) {
+        });
+    });
 }
+playVideo();
