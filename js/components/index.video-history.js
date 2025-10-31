d750a76c4129b45e0721a088b7b8946e8bf5da3b
Yuliia <julianna197@gmail.com>
2025-10-31 19:07:24 +0000

    Add js scripts

--- js/components/index.video-history.js
+++ js/components/index.video-history.js
@@ -8,21 +8,17 @@ function playVideo() {
             const videoText = this.querySelector('.history__video-text');
 
             if (videoId && backgroundVideoContainer) {
-                // Create iframe element for YouTube video
                 const iframe = document.createElement('iframe');
                 iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1`;
                 iframe.allow = 'autoplay; encrypted-media';
                 iframe.allowFullscreen = true;
                 iframe.classList.add('history__background-iframe');
 
-                // Clear existing content and append iframe
                 backgroundVideoContainer.innerHTML = '';
                 backgroundVideoContainer.appendChild(iframe);
 
-                // Add active class to show the video background
                 backgroundVideoContainer.classList.add('history__background-video--active');
 
-                // Change text when video is playing
                 if (videoText) {
                     videoText.textContent = 'Video Playing';
                 }
@@ -30,4 +26,5 @@ function playVideo() {
         });
     });
 }
+
 playVideo();
