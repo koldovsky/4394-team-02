45fb4585944174e720c7269df991c68d95f9d959
Yuliia <julianna197@gmail.com>
2025-10-31 19:47:01 +0000

    Play video

--- js/components/index.video-history.js
+++ js/components/index.video-history.js
@@ -1,5 +1,6 @@
 function playVideo() {
     const playVideoInBackground = document.querySelectorAll('.history__video-wrapper');
+    let isPlaying = false;
 
     playVideoInBackground.forEach(playButton => {
         playButton.addEventListener('click', function (event) {
@@ -8,19 +9,29 @@ function playVideo() {
             const videoText = this.querySelector('.history__video-text');
 
             if (videoId && backgroundVideoContainer) {
-                const iframe = document.createElement('iframe');
-                iframe.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1`;
-                iframe.allow = 'autoplay; encrypted-media';
-                iframe.allowFullscreen = true;
-                iframe.classList.add('history__background-iframe');
+                if (!isPlaying) {
+                    const iframe = document.createElement('iframe');
+                    iframe.src = `https://www.youtube.com/embed/${videoId}?enablejsapi=1&autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1&origin=${window.location.origin}`;
+                    iframe.allow = 'autoplay; encrypted-media; fullscreen';
+                    iframe.setAttribute('allowfullscreen', '');
+                    iframe.classList.add('history__background-iframe');
 
-                backgroundVideoContainer.innerHTML = '';
-                backgroundVideoContainer.appendChild(iframe);
+                    backgroundVideoContainer.innerHTML = '';
+                    backgroundVideoContainer.appendChild(iframe);
+                    backgroundVideoContainer.classList.add('history__background-video--active');
 
-                backgroundVideoContainer.classList.add('history__background-video--active');
+                    if (videoText) {
+                        videoText.textContent = 'Pause Video';
+                    }
+                    isPlaying = true;
+                } else {
+                    backgroundVideoContainer.innerHTML = '';
+                    backgroundVideoContainer.classList.remove('history__background-video--active');
 
-                if (videoText) {
-                    videoText.textContent = 'Video Playing';
+                    if (videoText) {
+                        videoText.textContent = 'Play Video';
+                    }
+                    isPlaying = false;
                 }
             }
         });
